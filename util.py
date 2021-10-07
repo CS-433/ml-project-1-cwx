0dbde0a795fe9b82cba5ed8fd391f93376cfc7b2
xuzewei28 <Xuzewei@hotmail.com>
2021-10-08 00:39:22 +0200

    fix

--- util.py
+++ util.py
@@ -20,16 +20,22 @@ class Module(object):
 class Linear(Module):
     def __init__(self, i_dim, o_dim):
         super(Linear, self).__init__()
-        e = 2.0 / np.sqrt(i_dim + o_dim)
-        self.w = np.random.normal(0, e, size=(o_dim, i_dim))
-        self.b = np.random.normal(0, e, size=o_dim)
+        e = 1 / np.sqrt(i_dim)
+        self.w = np.random.uniform(-e, e, (o_dim, i_dim))
+        self.b = np.random.uniform(-e, e, o_dim)
         self.dw = np.zeros(self.w.shape)
         self.db = np.zeros(self.b.shape)
         self.x = 0
+        self.m1 = np.zeros(self.w.shape)
+        self.v1 = np.zeros(self.w.shape)
+        self.m2 = np.zeros(self.b.shape)
+        self.v2 = np.zeros(self.b.shape)
 
-    def forward(self, x):
+    def forward(self, x, test=False):
         self.x = x
-        return (self.w @ (x.transpose())).transpose() + self.b
+        x = x.dot(self.w.transpose())
+        x = x + self.b
+        return x
 
     def backward(self, gradwrtoutput):
         gradwrtoutput = gradwrtoutput.transpose()
@@ -38,7 +44,7 @@ class Linear(Module):
         return (self.w.transpose().dot(gradwrtoutput)).transpose()
 
     def param(self):
-        return [[self.w, self.dw], [self.b, self.db]]
+        return [[self.w, self.dw, self.m1, self.v1], [self.b, self.db, self.m2, self.v2]]
 
     def zero_grad(self):
         self.dw = np.zeros(self.dw.shape)
@@ -50,7 +56,7 @@ class ReLU(Module):
         super(ReLU, self).__init__()
         self.x = None
 
-    def forward(self, x):
+    def forward(self, x, test=False):
         self.x = x
         x[x < 0] = 0
         return x
@@ -62,16 +68,48 @@ class ReLU(Module):
         return np.multiply(gradwrtoutput, x)
 
 
+class Tanh(Module):
+    def __init__(self):
+        super(Tanh, self).__init__()
+        self.x = None
+
+    def forward(self, x, test=False):
+        self.x = x
+        return np.tanh(x)
+
+    def backward(self, gradwrtoutput):
+        return 4 * (np.power(np.exp(self.x) + np.exp(-1 * self.x), -2)) * gradwrtoutput
+
+
+class Dropout(Module):
+    def __init__(self, prob=0.0):
+        super(Dropout, self).__init__()
+        self.mask = None
+        self.prob = prob
+
+    def forward(self, x, test=False):
+        if test:
+            return x
+        self.mask = np.random.binomial(1, self.prob, x.shape)
+        out = np.multiply(x, self.mask) / (1 - self.prob)
+        return out
+
+    def backward(self, gradwrtoutput):
+        return np.multiply(gradwrtoutput, self.mask) / (1 - self.prob)
+
+
 class MSE(Module):
     def __init__(self):
         self.error = None
+        self.n = None
 
     def forward(self, pred, label):
         self.error = pred - label
-        return np.sum(np.power(self.error, 2))
+        self.n = pred.shape[0]
+        return np.mean(np.power(self.error, 2))
 
     def backward(self):
-        return 2 * self.error
+        return self.error / self.n
 
 
 class SGD(Module):
@@ -94,10 +132,10 @@ class Sequential(Module):
     def add_module(self, module):
         self.modules.append(module)
 
-    def forward(self, input):
+    def forward(self, input, test=False):
         out = input
         for module in self.modules:
-            out = module.forward(out)
+            out = module.forward(out, test=False)
         return out
 
     def backward(self, grdwrtoutput):
@@ -125,24 +163,41 @@ def compute_error(target, prediction):
     return count
 
 
-def train_model(train_input, train_target, model, learning_rate=1e-4, nb_epochs=300, mini_batch_size=100):
+def train_model(train_input, train_target, model, lambda_l2=0, learning_rate=1e-1, nb_epochs=1000,
+                mini_batch_size=1000, print_res=False):
     criterion = MSE()
     optimizer = SGD(model, learning_rate)
-    for e in range(0, nb_epochs):
+    p1 = 0.9
+    p2 = 0.999
+    eps = 1e-8
+    index = np.arange(0, train_input.shape[0], 1, dtype=int)
+    for e in range(0, nb_epochs + 1):
         loss = 0
         a = 0
         b = mini_batch_size
-        while a < len(train_input):
+        while a < mini_batch_size:
             if b >= len(train_input):
                 b = len(train_input)
-            output = model.forward(train_input[a:b])
-            loss += criterion.forward(output, train_target[a:b])
+            output = model.forward(train_input[np.where(index % mini_batch_size == a)])
+            loss += criterion.forward(output, train_target[np.where(index % mini_batch_size == a)])
             model.zero_grad()
             model.backward(criterion.backward())
-            for (p, dp) in model.param():
+            for (p, dp, m, v) in model.param():
+                p *= (1 - lambda_l2)
                 p -= learning_rate * dp
-            a = b
+            a = a + 1
             b = b + mini_batch_size
+
+        if e % 10 == 0 and print_res:
+            print(e)
+            output = model.forward(x1)
+            predicted = np.ones((len(output), 1), dtype=int)
+            predicted[np.where(output[:, 0] > output[:, 1])] = 0
+            print("train", compute_error(y1, predicted) / x1.shape[0])
+            output = model.forward(x2)
+            predicted = np.ones((len(output), 1), dtype=int)
+            predicted[np.where(output[:, 0] > output[:, 1])] = 0
+            print("test", compute_error(y2, predicted) / x2.shape[0])
 
 
 train_path = "data/train.csv/train.csv"
@@ -153,21 +208,22 @@ _, text_data, ids = load_csv_data(test_path)
 
 train_data, text_data = fix_empty(train_data, text_data)
 train_data, text_data = standardize(train_data, text_data)
-
+train_data, text_data = pca(train_data, text_data)
 train_target = convert_to_one_hot(train_target)
 
 y1, x1, y2, x2 = train_target[:200000], train_data[:200000], train_target[200000:], train_data[200000:]
 
-model = Sequential(Linear(30, 64), ReLU(), Linear(64, 16), ReLU(), Linear(16, 16), ReLU(),Linear(16, 2))
+model = Sequential(Linear(x1.shape[1], 64), ReLU(), Linear(64, 16), ReLU(), Linear(16, 16), ReLU(),
+                   Linear(16, y1.shape[1]))
 
-train_model(train_data, train_target, model)
+train_model(train_data, train_target, model, print_res=True)
 
 output = model.forward(x1)
-predicted = np.ones((len(output), 1),dtype=int)
+predicted = np.ones((len(output), 1), dtype=int)
 predicted[np.where(output[:, 0] > output[:, 1])] = 0
 print("train_error", compute_error(y1, predicted) / y1.shape[0])
 
 output = model.forward(x2)
-predicted = np.ones((len(output), 1),dtype=int)
+predicted = np.ones((len(output), 1), dtype=int)
 predicted[np.where(output[:, 0] > output[:, 1])] = 0
 print("test_error", compute_error(y2, predicted) / y2.shape[0])
